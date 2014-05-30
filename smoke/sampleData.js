87e2ee5ce979488ccc4fc0c1788f2354e892d88b
frankiefu <frankiefu@yahoo.com>
2014-05-30 16:30:59 -0700

    initial commit

---
+++ smoke/sampleData.js
@@ -0,0 +1,73 @@
+(function() {
+  
+  window.categories = [
+    {
+      name: 'April Fools!',
+      badge: 'Prankster',
+      badgeScore: 2,
+      quizzes: [
+        {
+          type: 'fill-blank', 
+          question: 'Can you name the Google product from a 2006 April Fools day prank?',
+          start: 'Love is just another search problem. Solve it with Google',
+          end: '.',
+          answer: 'Romance'
+        },
+        {
+          type: 'multi-select',
+          question: 'Which of the following April Fools jokes has Google actually done?',
+          options: [
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs'
+          ],
+          answer: '0,1,2'
+        },
+        {
+          type: 'single-select',
+          question: 'Which real Google product launched on April Fools Day in 2004?',
+          options: [
+            'Drive',
+            'Calendar',
+            'Gmail',
+            'Chrome'
+          ],
+          answer: 2
+        }
+      ]
+    },
+    {
+      name: 'I/O Trivia', 
+      badge: 'I/O Expert',
+      badgeScore: 1,
+      quizzes: [
+        {
+          type: 'single-select', 
+          question: 'When was the first Google I/O?',
+          options: [
+            '2006',
+            '2007',
+            '2008',
+            '2014'
+          ],
+          answer: 2
+        },
+        {
+          type: 'multi-select',
+          question: 'What are the 3 I/O themes this year?',
+          options: [
+            'Doodle',
+            'Design',
+            'Debug',
+            'Develop',
+            'Distribute',
+            'Decode'
+          ],
+          answer: '1,3,4'
+        }
+      ]
+    }
+  ];
+  
+})();
