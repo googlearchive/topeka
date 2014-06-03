82f82f5acf57be8a8c2b7aed5528b9ef5554b466
frankiefu <frankiefu@yahoo.com>
2014-06-02 18:10:50 -0700

    update sampleData to show more questions and results

--- smoke/sampleData.js
+++ smoke/sampleData.js
@@ -20,6 +20,202 @@
             'Search for buried treasure in Maps',
             'Google was renamed Topeka',
             'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs'
+          ],
+          answer: '0,1,2'
+        },
+        {
+          type: 'single-select',
+          question: 'Which real Google product launched on April Fools Day in 2004?',
+          options: [
+            'Drive',
+            'Calendar',
+            'Gmail',
+            'Chrome'
+          ],
+          answer: 2
+        },
+        {
+          type: 'fill-blank', 
+          question: 'Can you name the Google product from a 2006 April Fools day prank?',
+          start: 'Love is just another search problem. Solve it with Google',
+          end: '.',
+          answer: 'Romance'
+        },
+        {
+          type: 'multi-select',
+          question: 'Which of the following April Fools jokes has Google actually done?',
+          options: [
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs'
+          ],
+          answer: '0,1,2'
+        },
+        {
+          type: 'single-select',
+          question: 'Which real Google product launched on April Fools Day in 2004?',
+          options: [
+            'Drive',
+            'Calendar',
+            'Gmail',
+            'Chrome'
+          ],
+          answer: 2
+        },
+        {
+          type: 'fill-blank', 
+          question: 'Can you name the Google product from a 2006 April Fools day prank?',
+          start: 'Love is just another search problem. Solve it with Google',
+          end: '.',
+          answer: 'Romance'
+        },
+        {
+          type: 'multi-select',
+          question: 'Which of the following April Fools jokes has Google actually done?',
+          options: [
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs'
+          ],
+          answer: '0,1,2'
+        },
+        {
+          type: 'single-select',
+          question: 'Which real Google product launched on April Fools Day in 2004?',
+          options: [
+            'Drive',
+            'Calendar',
+            'Gmail',
+            'Chrome'
+          ],
+          answer: 2
+        },
+        {
+          type: 'fill-blank', 
+          question: 'Can you name the Google product from a 2006 April Fools day prank?',
+          start: 'Love is just another search problem. Solve it with Google',
+          end: '.',
+          answer: 'Romance'
+        },
+        {
+          type: 'multi-select',
+          question: 'Which of the following April Fools jokes has Google actually done?',
+          options: [
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs'
+          ],
+          answer: '0,1,2'
+        },
+        {
+          type: 'single-select',
+          question: 'Which real Google product launched on April Fools Day in 2004?',
+          options: [
+            'Drive',
+            'Calendar',
+            'Gmail',
+            'Chrome'
+          ],
+          answer: 2
+        },
+        {
+          type: 'fill-blank', 
+          question: 'Can you name the Google product from a 2006 April Fools day prank?',
+          start: 'Love is just another search problem. Solve it with Google',
+          end: '.',
+          answer: 'Romance'
+        },
+        {
+          type: 'multi-select',
+          question: 'Which of the following April Fools jokes has Google actually done?',
+          options: [
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
+            'Replace youtube videos with animated gifs',
+            'Search for buried treasure in Maps',
+            'Google was renamed Topeka',
+            'Translate site text to emoji in chrome',
             'Replace youtube videos with animated gifs'
           ],
           answer: '0,1,2'
