59627a5a59b437cc7e6792bd072af9b5e7a4c0a8
Alex Russell <slightlyoff@chromium.org>
2014-06-16 01:21:31 -0700

    Some starts at attempts to deal with fonts. So far unsuccessful. Balls.

--- sw.js
+++ sw.js
@@ -9,15 +9,36 @@ this.addEventListener("install", function(e) {
   e.waitUntil(caches.create("core"));
 });
 
+
 this.addEventListener("fetch", function(e) {
+  var request = e.request;
+
+  /*
+  var baseUrl = this.scope.split("*")[0];
+  // If it's a request for the base font CSS file, redirect the request
+  // to the local copy
+  if (request.url.indexOf("http://fonts.googleapis.com/css") == 0) {
+    // e.redirectTo() isn't implemented yet
+    var headers = new HeaderMap();
+    headers.set("Location", baseUrl+"polyfills/fonts/fonts.css");
+    var r = new Response(null, {
+      status: 302,
+      statusText: "Found",
+      headers: headers,
+    });
+    e.respondWith(r);
+    return;
+  }
+  */
+
   // If it's off-origin, don't try to cache.
-  if (this.scope.indexOf(e.request.origin) == -1) {
+  if (this.scope.indexOf(request.origin) == -1) {
     return;
   }
 
   // Basic read-through caching.
   e.respondWith(
-    caches.match(e.request, "core").then(
+    caches.match(request, "core").then(
       function(response) {
         return response;
       },
@@ -25,7 +46,7 @@ this.addEventListener("fetch", function(e) {
         // we didn't have it in the cache, so add it to the cache and return it
         return caches.get("core").then(
           function(core) {
-            return core.add(e.request).then(
+            return core.add(request).then(
               function(response) {
                 return response;
               }
